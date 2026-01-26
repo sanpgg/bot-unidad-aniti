3f25aee5694c79fb4942120fd906b627093741ec
Jorge Sánchez <jorgesanchez1193@gmail.com>
2026-01-26 13:33:43 -0600

    Update denunciaAnonima.flow.ts
    Si es anónimo envía otro mensaje  Para garantizar el anonimato y la confidencialidad te invitamos a presentarla a través del Sistema Integral de Denuncias:\n\n https://denuncia.sanpedro.gob.mx/denuncia

--- src/flows/denuncia/denunciaAnonima.flow.ts
+++ src/flows/denuncia/denunciaAnonima.flow.ts
@@ -1,24 +1,21 @@
 import { addKeyword, utils } from "@builderbot/bot";
 import { Provider, Database } from "../../bot";
 import { DenunciaState, ASK_HECHOS } from "./denuncia.types";
-import { denunciaHechosFlow } from "./denunciaHechos.flow";
+import { menuFlow } from "../menu.flow";
+import { logOutgoingMessage } from "../../middlewares/messageLogger";
 
 export const denunciaAnonimaFlow = addKeyword<Provider, Database>(utils.setEvent("DENUNCIA_ANONIMA_FLOW"))
   .addAnswer(
-    "✅ Tu denuncia será *anónima*.",
+    "Para garantizar el anonimato y la confidencialidad te invitamos a presentarla a través del Sistema Integral de Denuncias:\n\n https://denuncia.sanpedro.gob.mx/denuncia",
     { capture: false },
-    async (ctx, { state, gotoFlow, fallBack }) => {
-
-         await state.update({
-            denuncia: {
-                tipo: "anonima",
-                adjuntos: [],
-                infoAdicional: [],
-            } as DenunciaState,
-        });
-        
-        
-        return gotoFlow(denunciaHechosFlow);
-
+    async (ctx, { flowDynamic, gotoFlow }) => {
+      await logOutgoingMessage(
+        ctx.from,
+        "Para garantizar el anonimato y la confidencialidad te invitamos a presentarla a través del Sistema Integral de Denuncias: https://denuncia.sanpedro.gob.mx/denuncia",
+        'denuncia_anonima'
+      );
+      
+      await flowDynamic("_Te enviaremos el menú principal para que puedas continuar con otras opciones:_");
+      return gotoFlow(menuFlow);
     }
   );
